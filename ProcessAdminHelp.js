f6b2bfc41ba92e06b64a359b36b47d5c473888ab
Tabea David <info@justonestep.de>
2016-02-23 18:04:27 +0100

    process js: adding some comments :)

--- ProcessAdminHelp.js
+++ ProcessAdminHelp.js
@@ -1,33 +1,42 @@
 /* global $ */
 
 $(document).ready(function () {
-  var hash = window.location.hash.substring(1);
+  var hash = location.hash.substring(1);
   var $selectedItem;
   var $body = $('html, body');
   var $list = $('#cbp-ntaccordion');
 
+  // call jquery plugin
   $list.cbpNTAccordion();
 
+  // if there is a hash, get related item,
+  // slide it up by adding specific class
+  // and scroll to that item
   if (hash) {
     $selectedItem = $list.find('li[name="' + hash + '"]');
 
     if ($selectedItem.length) {
       $selectedItem.addClass('cbp-ntopen');
 
+      // important: otherwise the item isn't expanded
+      // and scrollTop() won't work
       setTimeout(function () {
         $body.scrollTop($selectedItem.offset().top);
       }, 500);
     }
   }
 
+  // change hash depending on item
   $list.children().on('click.cbpNTAccordion', function () {
     var $this = $(this);
 
+    // change hash and scroll to item
     if ($this.hasClass('cbp-ntopen')) {
-      window.location.hash = $this.attr('name');
+      location.hash = $this.attr('name');
       $body.scrollTop($this.offset().top);
     } else {
-      window.location.hash = '';
+      // reset hash
+      location.hash = '';
     }
   });
 
